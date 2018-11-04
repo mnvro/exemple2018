880594342250a9a8c29a84ce9fc516ba589371fe
Marie-Noel Vroman <mn.vroman@ephec.be>
2018-11-04 22:06:14 +0100

    Sytem.out.println --> JOptionPane.showMessageDialog

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
+import javax.swing.JOptionPane;
+
 public class Hello{
 	public static void main (String args[]{
-		System.out.println("Bonjour tout le monde !");
+		JOptionPane.showMessageDialog(null,"Bonjour tout le monde !");
 	}
 }
